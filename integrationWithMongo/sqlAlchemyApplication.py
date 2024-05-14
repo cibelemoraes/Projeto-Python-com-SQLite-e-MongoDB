0686a1d137c024e6fd35665c86d45a92e7d4cbc6
Your Name <you@example.com>
2024-05-13 22:04:17 -0300

    sqlalchemy

--- integrationWithMongo/sqlAlchemyApplication.py
+++ integrationWithMongo/sqlAlchemyApplication.py
@@ -3,125 +3,129 @@
     utilizando SQLAlchemy e modelo ORM
     """
     
+# DESAFIO DIO - Integrando Python com SQLite e MongoDB
+
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column
 from sqlalchemy import create_engine
-from sqlalchemy import inspect
 from sqlalchemy import select
-from sqlalchemy import func
 from sqlalchemy import Integer
 from sqlalchemy import String
+from sqlalchemy import Float
 from sqlalchemy import ForeignKey
 
 
+# Declaração das Classes para o Modelo ORM
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "user_account"
-    #atributos
-    id = Column(Integer, primary_Key=True)
-    name = Column(String)
-    fullname = Column(String)
-    
-    address = relationship(
-        "Address", back_populates="user", cascade="all, delete-orpham"
-    )
 
-    def __repr__(self):
-        return f"User(id={self.id}, name={self.name}, fullname={self.fullname})"
-    
-class Address(Base):
-    __tablename__ = "address"
+class Client(Base):
+    """
+    Esta classe representa a tabela cliente dentro do SQlite.
+    """
+    __tablename__ = "client"
+    # atributos
     id = Column(Integer, primary_key=True)
-    email_address = Column(String(30), nullable=False)
-    user_id = Column(Integer, ForeignKey("user_account.id"), nullable=False)
-    
-    user = relationship("user", back_populates="address")
-    
-    def __repr__(self):
-        return f"Address(id={self.id}, email_address={self.email_address})"
-    
-    
-print(User.__tablename__)
-print(Address.__tablename__)
+    name = Column(String(30))
+    cpf = Column(String(9))
+    address = Column(String(30))
 
-# conexão com banco de dados
+    def __repr__(self):
+        return f"Client(id={self.id}, name={self.name}, address={self.address})"
+
+
+class Account(Base):
+    """
+     Esta classe representa a tabela account dentro do SQlite.
+    """
+    __tablename__ = "account"
+    # atributos
+    id = Column(Integer, primary_key=True)
+    tipo = Column(String(2))
+    agency = Column(Integer)
+    number = Column(Integer)
+    balance = Column(Float)
+    client_id = Column(Integer, ForeignKey("client.id"), nullable=False)
+
+    def __repr__(self):
+        return f"Account(id={self.id}, tipo={self.tipo}, saldo={self.balance})"
+
+# Realiza a conexão com o banco de dados
 engine = create_engine("sqlite://")
 
-#criando as classes como tabelas no banco de dados
+
+# Cria as classes como tabelas no banco de dados
 Base.metadata.create_all(engine)
 
-#depreciado -  será removido em futuro release
-# print(engine.table_name())
 
-#investigando o esquema de banco de dados 
-inspetor_engine = inspect(engine)
-print(inspetor_engine.has_table("user_account"))
-print(inspetor_engine.get_table_names())
-print(inspetor_engine.default_schema_name)
-
-
-#inserindo dados no banco de dados
+# Faz a persistência das Informações no Banco de Dados SQLite
 with Session(engine) as session:
-    juliana = User(
-        name = 'Juliana',
-        fullname= 'Juliana Mascarenhas',
-        address=[Address(email_address='julianam"email.com')]
-        
-    )
-    
-    sandy = User(
-        name = 'Sandy',
-        fullname= 'Sandy Cardoso',
-        address=[Address(email_address='sandyam"email.com'),
-                 Address(email_address='sandyc@email.org')]
-    )
-    
-    patrick = User (name='patrick', fullname='Patrick Cardoso')
-    
-    
-    #enviandopara o BD (persistência de dados)
-    session.add_all([juliana, sandy, patrick])
-    
+    piangelo = Client(name='Piangelo Diatore',
+                     cpf='845.486.987.99',
+                     address='Rua 1 de Roma, número 540'
+                     )
+
+    lucy = Client(name='Lucy Tutti',
+                   cpf='101.203.406.23',
+                   address='Rua do Pio, número 3'
+                   )
+
+    pietro = Client(name='Pietro de Lorenzo',
+                     cpf='771.452.669.97',
+                     address='Rua Boo Wong the Ha, número 73'
+                     )
+
+    account1 = Account(client_id='1',
+                     tipo='cc',
+                     agency=2800,
+                     number=230803,
+                     balance=18500
+                     )
+    account2 = Account(client_id='2',
+                     tipo='cp',
+                     agency=3001,
+                     number=290110,
+                     balance=87900
+                     )
+    account3 = Account(client_id='3',
+                       tipo='cc',
+                       agency=9003,
+                       number=320457,
+                       balance=4780
+                       )
+
+    # Envia as informações para o BD (persitência de dados)
+    session.add_all([piangelo, lucy, pietro])
+    session.add_all([account1, account2, account3])
     session.commit()
-    
-    stmt = select(User).where(User.name.in_(["juliana", 'sandy']))
-    print('Recuperando usuários a partir de condições de filtragem')
-    for user in session.scalars(stmt):
-        print(user)
-        
-    stmt_address = select(Address).where(Address.user_id.in_([2]))
-    print('\nRecuperando os endereços de email de Sandy')
-    for address in session.scallars(stmt_address):
-        print(address)
-        
-    stmt_order = select(User).order_by(User.fullname.desc())
-    print("\nRecuperando info de maneira ordenada")
-    for result in session.scalars(stmt_order):
-        print(result)
-        
-    stmt_join = select(User.fullname, Address.email_address).join_from(Address, User)
-    print("\n")
-    for result in session.scalars(stmt_join):
-        print(result)
-        
-    #print(select(User.fullname, Address.email_address).join_from(Address, User))
-    
-    connection = engine.connect()
-    result = connection.execute(stmt_join).fetchall()
-    print("\nExecutando statement a partirda connection")
-    for result in results:
-        print(result)
-        
-    stmt_count = select(func.count('*')).select_from(User)
-    print('\nTotal de instâncias em User')
-    for result in session.scalars(stmt_count):
-        print(result)
-        
-        
-    # encerrando de fato a session
-    session.closed()
-    
-    
+
+
+# Consulta as Informações Salvas no Banco de Dados SQLite
+
+print('Recuperando clientes a partir de uma condição de filtragem:')
+stmt_clients = select(Client).where(Client.name.in_(['Piangelo Diatore', 'Pietro de Lorenzo']))
+for result in session.scalars(stmt_clients):
+    print(result)
+
+
+print("\nRecuperando clientes de maneira ordenada:")
+stmt_order = select(Client).order_by(Client.name.desc())
+for result in session.scalars(stmt_order):
+    print(result)
+
+
+print("\nRecuperando contas de maneira ordenada:")
+stmt_accounts = select(Account).order_by(Account.tipo.desc())
+for result in session.scalars(stmt_accounts):
+    print(result)
+
+
+print("\nRecuperando contas e clientes:")
+stmt_join = select(Client.name, Account.tipo, Account.balance).join_from(Client, Account)
+connection = engine.connect()
+results = connection.execute(stmt_join).fetchall()
+for result in results:
+    print(result)
+
+session.close()
